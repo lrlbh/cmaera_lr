28f856615dbc46ce1df8f4bc9a942eabd36dc6fe
lrlhb <825422719@qq.com>
2026-09-04 01:31:13 +0800

    删除yz.py功能
    实现部分功能不如完全不实现，老是需要看某功能是否存在

--- py_mod/lib_lsl/yz.py
+++ py_mod/lib_lsl/yz.py
@@ -3,112 +3,37 @@ import _thread
 
 class YZ:
     def __init__(self, value):
-        self._value = value
+        self.value = value
         self._lock = _thread.allocate_lock()
 
-    def lock(self):
-        self._lock.acquire()
+    # 未曾实现方法转接到self.value中,但不加锁
+    def __getattr__(self, name):
+        def wrapper(*args):
+            # with self._lock:
+            attr = getattr(self.value, name)
+            return attr(*args)
+
+        return wrapper
+
+    # #################引出锁让外部访问################
+    def is_lock(self):
+        return self._lock.locked()
+
+    def lock(self, 阻塞=1):
+        # 无法查到资料，非0都应该是阻塞
+        # 无超时参数
+        self._lock.acquire(阻塞)
 
     def rlock(self):
         self._lock.release()
 
-    # 比较并交换,当前值==eq_value,则当前值=new_value
-    def eq_and_set(self, eq_value, new_value):
-        with self._lock:
-            if self._value == eq_value:
-                self._value = new_value
-                return True
-            return False
+    # 简单实现，用with时不能访问该类的函数了
+    # 由于有__getattr__，用内部原始成员变量，原始访问
+    def __enter__(self):
+        self._lock.acquire()
+        return self  # 返回self,外部用 as x,就可以访问到self
 
-    # 执行未曾实现的方法
-    def __getattr__(self, name):
-        def wrapper(*args):
-            with self._lock:
-                attr = getattr(self._value, name)
-                return attr(*args)
-
-        return wrapper
-
-    # list追加
-    def append(self, value):
-        with self._lock:
-            self._value.append(value)
-
-    # list pop
-    def pop(self, n):
-        with self._lock:
-            return self._value.pop(n)
-
-    # dict更新
-    def update(self, value):
-        with self._lock:
-            self._value.update(value)
-
-    # ################无法重载的###################
-    def get(self):
-        with self._lock:
-            return self._value
-
-    def set(self, value):
-        with self._lock:
-            self._value = value
-
-    #################中括号,需要注意返回值是引用情况###################
-    def __getitem__(self, index):
-        with self._lock:
-            return self._value[index]
-
-    def __setitem__(self, index, value):
-        with self._lock:
-            self._value[index] = value
-
-    def __delitem__(self, index):
-        with self._lock:
-            del self._value[index]
-
-    #################无歧义运算符############################
-    # +=
-    def __iadd__(self, other):
-        with self._lock:
-            self._value += other
-        return self
-
-    # -=
-    def __isub__(self, other):
-        with self._lock:
-            self._value -= other
-        return self
-
-    # *=
-    def __imul__(self, other):
-        with self._lock:
-            self._value *= other
-        return self
-
-    # /=
-    def __itruediv__(self, other):
-        with self._lock:
-            self._value /= other
-        return self
-
-    # //=
-    def __ifloordiv__(self, other):
-        with self._lock:
-            self._value //= other
-        return self
-
-    # %=
-    def __imod__(self, other):
-        with self._lock:
-            self._value %= other
-        return self
-
-    # **=
-    def __ipow__(self, other):
-        with self._lock:
-            self._value **= other
-        return self
-
-    # #################当前锁状态############################
-    def locked(self):
-        return self._lock.locked()
+    def __exit__(self, exc_type, exc_value, traceback):
+        # 假设了release不会出现错误
+        self._lock.release()
+        return False  # 不吞异常
