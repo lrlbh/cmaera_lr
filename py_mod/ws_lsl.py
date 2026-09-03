39a87a941ed7c20af46c5037b51bbf6e82cb81f7
lrlhb <825422719@qq.com>
2026-09-04 01:37:34 +0800

    ws添加了读取功能

--- py_mod/ws_lsl.py
+++ py_mod/ws_lsl.py
@@ -7,16 +7,63 @@ import hashlib
 import time
 
 import lib_lsl
+import lib_lsl.tl
 
 
+# 基于阻塞的WS套接字
 class WsSocket:
-    def __init__(self, socket: socket.socket):
-        self.socket = socket
+    def __init__(self, socket_in: socket.socket, max_msg_len=6 * 1024 * 1024):
+        self.socket: socket.socket = socket_in
         self.timeout = None
+        self.max_msg_len = max_msg_len
+        self.head_buf = bytearray(8)
+        self.is_close = False
 
     # 转接 socket 中的方法
     def __getattr__(self, name):
         return getattr(self.socket, name)
+
+    # mpy判断是否close不方便,覆盖close
+    def close(self):
+        self.is_close = True
+        self.socket.close()
+
+    @staticmethod
+    def socket_close(func):
+        # 是否需要可以关闭日志打印？
+
+        func_name = func.__name__
+
+        def wrapper(self, *args, **kwargs):
+
+            # 无法获取到参数名称，也无法确定addr长度
+            # 所以假设它第2个参数，同时为元组
+            if len(args) >= 2 and isinstance(args[1], tuple):
+                addr = args[1]
+            else:
+                addr = "addr参数无法识别! 需要是第二个参数同时为元组"
+
+            try:
+                try:
+                    # 执行函数
+                    ret = func(self, *args, **kwargs)
+                    lib_lsl.send(f"{func_name} --> {addr} --> 正常退出函数")
+                    return ret  # 放心finally会在返回前被执行的
+
+                except OSError as e:
+                    lib_lsl.send(
+                        f"{func_name} --> {addr} --> 退出: {lib_lsl.tl.get_完整错误信息(e)}"
+                    )
+
+                finally:
+                    self.close()
+
+            except Exception as e:
+                lib_lsl.send_err(
+                    f"{func_name} --> {addr} --> 退出: {lib_lsl.tl.get_完整错误信息(e)}"
+                )
+
+        return wrapper
 
     def send_ws(self, data):
 
@@ -31,6 +78,7 @@ class WsSocket:
         else:
             frame.append(0x82)
 
+        # str 编码在后计算len
         data_len = len(data)
 
         # ===== 第二个字节 + 扩展长度 =====
@@ -53,16 +101,15 @@ class WsSocket:
     # 返回一个编码后可以直接发送的bytes
     @staticmethod
     def get_msg(data):
+        frame = bytearray()
+
         if isinstance(data, str):
             data = data.encode()
-            opcode = 0x81
+            frame.append(0x81)
         else:
-            opcode = 0x82
+            frame.append(0x82)
 
         data_len = len(data)
-
-        frame = bytearray()
-        frame.append(opcode)
 
         if data_len <= 125:
             frame.append(data_len)
@@ -77,10 +124,131 @@ class WsSocket:
 
         return bytes(frame)
 
-    #  None(阻塞), 0(非阻塞), 或浮点数(秒)
+        #  None(阻塞), 0(非阻塞), 或浮点数(秒)
+
     def settimeout_lr(self, timeout):
         self.timeout = timeout
         self.settimeout(timeout)
+
+    def read_ws(self, buf=None):
+        """
+        字符串消息
+            总是返回新的字符串
+            但buf可以当作缓存加速
+            buf长度不够行为也和bin一样
+        bin消息
+            buf != nil
+                buf长度足够
+                    返回memoryview
+                buf长度不够
+                    扩展用户buf?
+                    返回新的bytearray()?
+                    抛出错误显式处理？----暂时定此种处理方式
+            buf == nil
+                返回新的bytearray()
+        --------------------------------------------------
+        是否需要恢复错误？大部分错误恢复很简单
+        重连一下完事了，吃饱了没事干,在外面判断一堆错误
+        抛出,及时记录,修复,当前场景下明显更方便
+        """
+
+        # ws保证单条任何类型消息至少有2字节
+        lib_lsl.tl.readinto_exact(self.socket, self.head_buf, 2)
+
+        # fin
+        if (self.head_buf[0] & 0x80) == 0:
+            # 在这里抛出情况下，此错误上下文错乱，不要恢复
+            raise Exception("WS没实现消息分片")
+
+        if self.head_buf[0] & 0x70:
+            raise Exception("WS非标协议 RSV位非0")
+
+        #  Opcode
+        opcode = self.head_buf[0] & 0x0F
+
+        # pong和 close都应该被当作一条完整的消息处理
+        # ----------------------------------------------
+        # 我查到的标准流程是  收到close --> 回复close --> server主动.tcp.close
+        # 瞎几把扯啊，不可能啊，应该需要发送close的主动调用tcp.close才对吧
+        # 即时一定要服务器先关闭，也应该这样才对
+        # 1、close --> 回复close -->服务器先半关闭-->等待客服端也半关闭然后--> server.tcp.close
+        # 2、查下资料tcp必须保证，close的数据必须在缓冲区清空后才会发送
+        # ----------------------------------------------
+        # 如果要回复的话必然要加锁，并且禁止用户访问tcp套接字发送数据了，有时间再写
+        if opcode == 0x8:
+            # 直接用此种方式，对方的close事件应该会被异化为ERROR事件
+            self.close()
+            raise Exception("收到ws_close,直接断开了TCP")
+
+        # 不处理其他消息
+        if opcode != 0x1 and opcode != 0x2:
+            # 在这里抛出情况下，此错误上下文错乱，不要恢复
+            raise Exception(
+                f"WS只支持str、bytes、colose(伪)消息类型! opcode: {opcode:#x}"
+            )
+
+        # 解析消息类型
+        is_str = opcode == 0x1
+
+        # 解析是否有掩码,允许客服端发送无掩码数据
+        is_masked = (self.head_buf[1] & 0x80) != 0
+
+        # 解析消息长度
+        msg_len = self.head_buf[1] & 0x7F
+        # 扩展长度下,长度不应该小于125(?),没有校验
+        if msg_len == 126:
+            lib_lsl.tl.readinto_exact(self.socket, self.head_buf, 2)
+            msg_len = int.from_bytes(self.head_buf[0:2], "big")
+        elif msg_len == 127:
+            lib_lsl.tl.readinto_exact(self.socket, self.head_buf, 8)
+            if self.head_buf[0] & 0x80:
+                # 在这里抛出情况下，此错误上下文错乱，不要恢复
+                raise Exception("非法WS消息长度,64bit需要最高位为0")
+            msg_len = int.from_bytes(self.head_buf, "big")
+
+        if msg_len > self.max_msg_len:
+            # 在这里抛出情况下，此错误上下文错乱，不要恢复
+            raise Exception("ws消息长度,大于允许长度")
+
+        # 读取掩码 Key
+        if is_masked:
+            mask_key = lib_lsl.tl.read_exact(self.socket, 4)
+
+        # 读取数据
+        if buf is None:
+            msg = lib_lsl.tl.read_exact(self.socket, msg_len)
+        elif len(buf) < msg_len:
+            # 在这里抛出情况下，此错误上下文错乱，不要恢复
+            raise Exception("ws长度大于传入buf")
+        else:
+            lib_lsl.tl.readinto_exact(self.socket, buf, msg_len)
+            msg = memoryview(buf)[:msg_len]
+
+        # 如果有掩码，进行异或解码
+        if is_masked:  # and is_str
+            for i in range(msg_len):
+                msg[i] ^= mask_key[i & 3]
+
+            # 测一下能否更快，若是有瓶颈写一个C函数处理
+            # m0, m1, m2, m3 = mask_key
+            # end = msg_len & ~3
+            # for i in range(0, end, 4):
+            #     msg[i] ^= m0
+            #     msg[i + 1] ^= m1
+            #     msg[i + 2] ^= m2
+            #     msg[i + 3] ^= m3
+            # for i in range(end, msg_len):
+            #     msg[i] ^= mask_key[i & 3]
+
+        # 转换为对应的数据类型返回
+        if is_str:
+            try:
+                # return msg.decode()
+                return str(msg, "utf8")
+            except UnicodeError:
+                raise Exception("WS收到字符串消息,但解码为utf-8失败")
+        else:
+            return msg
 
     def read_ws_temp(self):
         """
@@ -176,6 +344,7 @@ class t套接字上限行为:
     等待 = 1
 
 
+# 没有提供async版本，因为协程版本tcp速率显著更慢，可能协程API实现有问题
 class Server:
     def __init__(self, ip, port, listen):
         if ":" in ip:
@@ -210,7 +379,7 @@ class Server:
             try:
                 conn, addr = self.accept_all()
             except OSError as e:
-                if e.errno != 23:
+                if e.errno != 23:  # 原因非套接字上限,继续抛出
                     raise
                 if 套接字上限 == t套接字上限行为.重启:
                     machine.reset()
@@ -228,7 +397,7 @@ class Server:
     # 启动自动获取连接线程
     # rst,没有套接字资源时是否重启
     def run_thr(self, 套接字上限=t套接字上限行为.等待):
-        _thread.start_new_thread(self._work, (套接字上限,))
+        lib_lsl.Thread.create_thread(self._work, (套接字上限,))
         return self
 
     # 处理 ws和http_get 请求头
@@ -247,7 +416,7 @@ class Server:
                 if len(self.buf) >= 4 and self.buf[-4:] == b"\r\n\r\n":
                     self.conn.settimeout(None)
                 return
-            except:  # noqa: E722
+            except:
                 pass
 
             # 不处理的连接关闭
@@ -290,9 +459,8 @@ class Server:
 
                 # 没有确认是否是http
                 return self.conn, self.addr
-            except Exception as e:  
+            except Exception:
                 # print(e)
-                lib_lsl.send(e)
                 self.conn.close()
 
     # 手动获取一个ws连接,丢掉其他连接
